d4790334029725fcd2c345c6a1061a9d6b297ee3
Adrian Kuca <adriankuca2@gmail.com>
2022-04-22 22:44:25 +0200

    Further training improvements (dataset and global variables)

--- coregi/train.py
+++ coregi/train.py
@@ -6,18 +6,25 @@ from matplotlib import pyplot as plt
 import tensorflow as tf
 from configuration.paths import TRAINING_PATH, RESULTS_PATH
 
+EPOCHS = 100
 SIZE = 60
-DATASET = f"ndnq{SIZE}x{SIZE}"
+BATCH_SIZE = 64
+SHUFFLE_BUFFER_SIZE = 20000
+DATASET = f"nf{SIZE}x{SIZE}"
 NOW = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
 results_path = RESULTS_PATH(DATASET) / NOW
-checkpoints_path = RESULTS_PATH(DATASET) / NOW / "checkpoints"
+checkpoints_path = RESULTS_PATH(DATASET) / "checkpoints"
 
 print("Getting training dataset")
 train_images, train_labels = get_dataset(TRAINING_PATH(DATASET), "training")
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
 
 print("Getting testing dataset")
 test_images, test_labels = get_dataset(TRAINING_PATH(DATASET), "testing")
+test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(
+    BATCH_SIZE
+)
 
 
 print("Compiling model")
@@ -34,23 +41,23 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 )
 if tf.train.latest_checkpoint(checkpoints_path) != None:
     if input(tf.train.latest_checkpoint(checkpoints_path) + " load? y/n") == "y":
+        print("loading...")
         model.load_weights(tf.train.latest_checkpoint(checkpoints_path))
 
+dataset_batch = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 history = model.fit(
-    train_images,
-    train_labels,
-    epochs=2,
+    dataset_batch,
+    epochs=EPOCHS,
     callbacks=[checkpoint_callback],
-    validation_data=(test_images, test_labels),
 )
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+test_loss, test_acc = model.evaluate(test_dataset, verbose=2)
 print(test_acc)
 
 # Save history and model file for further analysys
 history.history["test_loss"] = test_loss
 history.history["test_acc"] = test_acc
 jsoned_history = json.dumps(history.history, indent=4)
-
 
 os.makedirs(results_path, exist_ok=True)
 with open(results_path / "history.json", "w") as f:
