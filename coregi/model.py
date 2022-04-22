0b996d51be92b9f4a79817228c5a205479a63125
Adrian Kuca <adriankuca2@gmail.com>
2022-04-22 17:34:59 +0200

    Tweak model randomly

--- coregi/model.py
+++ coregi/model.py
@@ -1,17 +1,31 @@
-# https://www.tensorflow.org/tutorials/images/cnn
-from tensorflow.keras import layers, models, datasets
+from tensorflow.keras import layers, models
+from configuration.datasets import labels_in_dataset
 
-input_size = 30
-model = models.Sequential()
-model.add(
-    layers.Conv2D(
-        input_size, (3, 3), activation="relu", input_shape=(input_size, input_size, 1)
+filter_size = (5, 5)
+pool_size = (2, 2)
+
+
+def get_model(input_size):
+    model = models.Sequential()
+    model.add(
+        layers.Conv2D(
+            input_size,
+            filter_size,
+            activation="relu",
+            input_shape=(input_size, input_size, 1),
+        )
     )
-)
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(input_size * 2, (3, 3), activation="relu"))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(input_size * 2, (3, 3), activation="relu"))
-model.add(layers.Flatten())
-model.add(layers.Dense(input_size * 2, activation="relu"))
-model.add(layers.Dense(10))
+    model.add(layers.Conv2D(input_size * 2, filter_size, activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size))
+    model.add(layers.Conv2D(input_size * 2, filter_size, activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size))
+    model.add(layers.Conv2D(input_size * 2, filter_size, activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size))
+    model.add(layers.Conv2D(input_size * 2, filter_size, activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size))
+    model.add(layers.Conv2D(input_size * 2, filter_size, activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(input_size * 2, activation="relu"))
+    model.add(layers.Dense(len(labels_in_dataset)))
+    return model
