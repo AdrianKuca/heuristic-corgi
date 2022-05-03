f2199b93c5243da565b5b26de21dde5860e6ecbc
Adrian Kuca <adriankuca2@gmail.com>
2022-05-03 16:50:29 +0200

    Still WIP confusion matrix plot

--- plots/confusion_matrix.py
+++ plots/confusion_matrix.py
@@ -1,39 +1,66 @@
+import importlib
 import os
 import tensorflow as tf
 from pathlib import Path
-from coregi.model import get_model
+from matplotlib import pyplot as plt
+from plots.utils import get_model_info, get_model_name, load_model_module
 from preprocessing.data import get_dataset
 from configuration.paths import DATA_PATH, TRAINING_PATH, RESULTS_PATH
+import multiprocessing as mp
+import subprocess
 
-checkpoints_path = RESULTS_PATH(DATASET) / "checkpoints"
 
-
-def load_dataset(input_size):
+def load_dataset(dataset):
     print("Getting testing dataset")
-    BATCH_SIZE = 64
-    test_images, test_labels = get_dataset(TRAINING_PATH(DATASET), "testing")
-    test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(
-        BATCH_SIZE
-    )
+    test_images, test_labels = get_dataset(TRAINING_PATH(dataset), "testing")
+    return test_images, test_labels
 
 
-def load_model(input_size):
+def worker(model_file, input_size, checkpoints_path, test_images, responses):
     print("Compiling model")
 
-    model = get_model(SIZE)
-    model.compile(
-        optimizer="adam",
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
+    spec = importlib.util.spec_from_file_location("model", str(model_file))
+    model_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(model_module)
+    model = model_module.get_model(input_size)
+    model.summary()
+    if tf.train.latest_checkpoint(checkpoints_path) != None:
+        print("loading...")
+        model.load_weights(tf.train.latest_checkpoint(checkpoints_path))
+        print("running...")
+        for image in test_images:
+            responses.append(model(image))
+        print("responsed!")
+
+
+def run_model(model_file, input_size, checkpoints_path, test_images):
+    manager = mp.Manager()
+    responses = manager.list()
+    process = mp.Process(
+        target=worker,
+        args=[model_file, input_size, checkpoints_path, test_images, responses],
+    )
+    process.start()
+    process.join()
+    process.close()
+    if len(responses):
+        return responses[0]
+
+
+def plot_confusion_matrix(labels, predictions, model_name):
+    confusion = tf.math.confusion_matrix(
+        labels=labels, predictions=predictions, num_classes=120
     )
 
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoints_path / "{loss}", save_weights_only=True
-    )
-    if tf.train.latest_checkpoint(checkpoints_path) != None:
-        if input(tf.train.latest_checkpoint(checkpoints_path) + " load? y/n") == "y":
-            print("loading...")
-            model.load_weights(tf.train.latest_checkpoint(checkpoints_path))
+    fig = plt.figure(figsize=(10, 10), tight_layout=True)
+    gs = fig.add_gridspec(1, 1)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax1.set_title
+    ax1.hist(confusion, bins=120)
+    ax1.legend(loc="center left")
+    dir = Path(Path(__file__).parent, "confusion_matrix")
+    dir.mkdir(parents=True, exist_ok=True)
+    plt.savefig(Path(dir, model_name + ".png"))
 
 
 if __name__ == "__main__":
@@ -45,4 +72,24 @@ if __name__ == "__main__":
                 for datedir in os.listdir(Path(root, dir)):
                     history_file = Path(root, dir, datedir, "history.json")
                     model_file = Path(root, dir, datedir, "model.py")
-                    input_size = int(dir.split("_")[1].split("x")[1])
+                    dataset_name = dir.split("_")[1]
+                    input_size = int(dataset_name.split("x")[1])
+                    checkpoints_path = Path(root, dir, datedir, "checkpoints")
+                    if (
+                        checkpoints_path.exists()
+                        and history_file.exists()
+                        and model_file.exists()
+                    ):
+                        test_images, test_labels = load_dataset(dataset_name)
+                        responses = run_model(
+                            model_file, input_size, checkpoints_path, test_images
+                        )
+                        if responses:
+                            model_module = load_model_module(model_file)
+                            plot_confusion_matrix(
+                                test_labels,
+                                responses,
+                                get_model_name(model_module, input_size),
+                            )
+                        else:
+                            print("Not drawing...")
